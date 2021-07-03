13d519f4153d2b974ce9a8cdb0d6179f5d85d5e0
murad alshorman <muradalshorman@gmail.com>
2021-07-03 18:44:44 +0300

    linkedlist

---
+++ node.js
@@ -0,0 +1,58 @@
+'use strict';
+
+class Node {
+  constructor(value){
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class LinkedList{
+  constructor() {
+    this.head = null;
+  }
+
+
+  insert(value){
+    if(!value){
+      throw new Error('nothing');
+    }
+    let current = new Node(value);
+    current.next = this.head;
+    this.head = current;
+  }
+
+  includes(value){
+    let current = this.head;
+    if(!value){
+      throw new Error('Improper value passed as argument');
+    }
+    if(!current){
+      throw new Error('Linked List invalid');
+    }
+    while(current){
+      if(current.value === value){
+        return true;
+      }
+      current = current.next;
+    }
+    return false;
+  }
+
+  toString(){
+    let current = this.head;
+    if(!current){
+      throw new Error('Linked List invalid');
+    }
+    let finalStr = '';
+    while(current){
+      finalStr += `{ ${current.value} } -> `;
+      current = current.next;
+    }
+    return finalStr += 'NULL';
+  }
+}
+  module.exports = {
+    ll: LinkedList,
+    node: Node
+  };
